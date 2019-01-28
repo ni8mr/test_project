b7f6952446749330f98ec19fe6c6b0d0e115321f
reza <noor.reza@circlefintech.com>
2019-01-28 16:32:03 +0600

    Add a form to add new topic.

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from .models import Board
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -15,3 +16,23 @@ def board_topics(request, pk):
     except Board.DoesNotExist:
         raise Http404
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() # TODO: Need to get the currently logged in user
+
+        topic = Topic.objects.create(subject=subject, board=board, starter=user)
+
+        post = Post.objects.create(message=message, topic=topic, created_by=user)
+        return redirect('board_topics', pk=board.pk) # TODO: Need to redirect to the created topic page
+
+    return render(request, 'new_topic.html', {'board': board})
