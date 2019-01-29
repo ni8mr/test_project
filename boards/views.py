c37aa0b61ed3c6e8e84fda6a485dd568b358db5e
reza <noor.reza@circlefintech.com>
2019-01-29 13:39:56 +0600

    Add django form.

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
 
@@ -23,16 +24,21 @@ def new_topic(request, pk):
         board = Board.objects.get(pk=pk)
     except Board.DoesNotExist:
         raise Http404
+    user = User.objects.first()  # TODO: Need to get the currently logged in user
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first() # TODO: Need to get the currently logged in user
-
-        topic = Topic.objects.create(subject=subject, board=board, starter=user)
-
-        post = Post.objects.create(message=message, topic=topic, created_by=user)
-        return redirect('board_topics', pk=board.pk) # TODO: Need to redirect to the created topic page
-
-    return render(request, 'new_topic.html', {'board': board})
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter =  user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: Need to redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
--- test_project/settings.py
+++ test_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'widget_tweaks',
+    # Application apps
     'boards'
 ]
 
